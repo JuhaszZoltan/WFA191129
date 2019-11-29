1cc6df0809b42b746b6f62afb8ccbb143d3ee480
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2019-11-29 09:30:23 +0100

    Add project files.

---
+++ WFA191129/FrmMain.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WFA191129
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvPkmn = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbnUj = new System.Windows.Forms.Button();
+            this.tbNev = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbTipus = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTorol = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPkmn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPkmn
+            // 
+            this.dgvPkmn.AllowUserToAddRows = false;
+            this.dgvPkmn.AllowUserToDeleteRows = false;
+            this.dgvPkmn.AllowUserToResizeColumns = false;
+            this.dgvPkmn.AllowUserToResizeRows = false;
+            this.dgvPkmn.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPkmn.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPkmn.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.dgvPkmn.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPkmn.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvPkmn.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvPkmn.Location = new System.Drawing.Point(12, 12);
+            this.dgvPkmn.Name = "dgvPkmn";
+            this.dgvPkmn.RowHeadersVisible = false;
+            this.dgvPkmn.RowHeadersWidth = 51;
+            this.dgvPkmn.RowTemplate.Height = 24;
+            this.dgvPkmn.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPkmn.Size = new System.Drawing.Size(581, 297);
+            this.dgvPkmn.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 20F;
+            this.Column1.HeaderText = "id";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "név";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.FillWeight = 70F;
+            this.Column3.HeaderText = "típus";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            // 
+            // tbnUj
+            // 
+            this.tbnUj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbnUj.Location = new System.Drawing.Point(17, 406);
+            this.tbnUj.Name = "tbnUj";
+            this.tbnUj.Size = new System.Drawing.Size(360, 49);
+            this.tbnUj.TabIndex = 1;
+            this.tbnUj.Text = "új pokemon";
+            this.tbnUj.UseVisualStyleBackColor = true;
+            this.tbnUj.Click += new System.EventHandler(this.tbnUj_Click);
+            // 
+            // tbNev
+            // 
+            this.tbNev.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbNev.Location = new System.Drawing.Point(17, 359);
+            this.tbNev.Name = "tbNev";
+            this.tbNev.Size = new System.Drawing.Size(171, 30);
+            this.tbNev.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Név:";
+            // 
+            // tbTipus
+            // 
+            this.tbTipus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbTipus.Location = new System.Drawing.Point(206, 359);
+            this.tbTipus.Name = "tbTipus";
+            this.tbTipus.Size = new System.Drawing.Size(171, 30);
+            this.tbTipus.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(201, 331);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Típus:";
+            // 
+            // btnTorol
+            // 
+            this.btnTorol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnTorol.Location = new System.Drawing.Point(400, 331);
+            this.btnTorol.Name = "btnTorol";
+            this.btnTorol.Size = new System.Drawing.Size(193, 124);
+            this.btnTorol.TabIndex = 1;
+            this.btnTorol.Text = "kijelölt törlése";
+            this.btnTorol.UseVisualStyleBackColor = true;
+            this.btnTorol.Click += new System.EventHandler(this.btnTorol_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 467);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTipus);
+            this.Controls.Add(this.tbNev);
+            this.Controls.Add(this.btnTorol);
+            this.Controls.Add(this.tbnUj);
+            this.Controls.Add(this.dgvPkmn);
+            this.Name = "FrmMain";
+            this.Text = "PKMN";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPkmn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPkmn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button tbnUj;
+        private System.Windows.Forms.TextBox tbNev;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbTipus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTorol;
+    }
+}
+
---
+++ WFA191129/FrmMain.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WFA191129
+{
+    public partial class FrmMain : Form
+    {
+        MySqlConnection conn;
+        public FrmMain()
+        {
+            conn = new MySqlConnection(
+                "Server=winsql.vereb.dc;" +
+                "Database=diak90;" +
+                "Uid=diak90;" +
+                "Pwd=2JtORd;");
+            InitializeComponent();
+            FillDgv();
+        }
+
+        private void FillDgv()
+        {
+            dgvPkmn.Rows.Clear();
+            conn.Open();
+            var cmd = new MySqlCommand("SELECT * FROM pokemonok;", conn);
+            var r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                dgvPkmn.Rows.Add(r[0], r[1], r[2]);
+            }
+            conn.Close();
+        }
+
+        private void tbnUj_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            var a = new MySqlDataAdapter()
+            {
+                InsertCommand = new MySqlCommand("INSERT INTO pokemonok (nev, tipus) VALUES " +
+                $"('{tbNev.Text}', '{tbTipus.Text}');", conn),
+            };
+            a.InsertCommand.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("tábla frissítve!");
+            FillDgv();
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            var a = new MySqlDataAdapter()
+            {
+                DeleteCommand = new MySqlCommand("DELETE FROM pokemonok WHERE " +
+                $"id = {dgvPkmn.SelectedRows[0].Cells[0].Value}", conn),
+            };
+            a.DeleteCommand.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("tábla frissítve!");
+            FillDgv();
+        }
+    }
+}
